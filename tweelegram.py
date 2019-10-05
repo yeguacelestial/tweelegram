2bcf593f818642489955904d6a00b885880e07ce
Yegua Celestial <rexcolt@localhost.localdomain>
2019-10-05 16:10:09 -0400

    Fixed encoding. Now it sends emojis, accents and shit.

--- tweelegram.py
+++ tweelegram.py
@@ -2,18 +2,19 @@ import json       # json: Se usa para analizar las respuestas de Telegram y las 
 import requests   # requests: Hace peticiones web usando Python, se usa para interactuar con la API de Telegram
 import time       # time: Utilizado para retrasos entre cada petición
 import tweepy     # tweepy: Librería de Twitter
+import urllib     # urllib: se usa para arreglar el problema de encoding
 
 # Variables
 
     #Telegram:
-TOKEN = "TelegramTokenHere"
+TOKEN = "TelegramBotToken"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
     #Twitter:
-consumer_key = 'YourConsumerKeyHere'
-consumer_secret = 'ConsumerSecretHere'
-access_token = 'AccesTokenHere'
-acces_token_secret = 'AccessTokenSecretHere'
+consumer_key = 'ConsumerKeyToken'
+consumer_secret = 'ConsumerSecretToken'
+access_token = 'AccessToken'
+acces_token_secret = 'AccessTokenSecret'
 
 def get_url(url):                                   # Descarga el contenido de una URL y se obtiene una string.
     response = requests.get(url)
@@ -26,9 +27,9 @@ def get_json_from_url(url):             # Toma el string de arriba y lo pasa a u
     return js
 
 def get_updates(offset=None):                      # Llama al comando de actualización de la API, y regresa una lista de
-    url = URL + "getUpdates"            #  "updates", los cuales son los mensajes enviados al bot.
+    url = URL + "getUpdates?timeout=100"            #  "updates", los cuales son los mensajes enviados al bot.
     if offset:
-        url += "?offset={}".format(offset)      # Indica a la API que ya no se desea recibir update_id menor al del update recibido.
+        url += "&offset={}".format(offset)      # Indica a la API que ya no se desea recibir update_id menor al del update recibido.
     js = get_json_from_url(url)
     return js
 
@@ -40,6 +41,7 @@ def get_last_chat_id_and_text(updates):     # Da información del id del chat y 
     return (text, chat_id)
 
 def send_message (text, chat_id):       # Toma el mensaje que se desea enviar (text) y el chat id al que queremos  enviar el mensaje. (chat_id)
+    text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)  # Llama al comando de la API, y le envía el mensaje.
     get_url(url)    # Se manda la nueva URL a la funcion "get_url"
 
